18dce86df36512e20bf6a7f554b79d045692416b
Philip Kim <filshouse56@gmail.com>
2023-12-18 15:17:49 -0500

    Exersice Completed

---
+++ Classes/Cars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Cars
+    {
+        public Cars() 
+        { 
+        
+        }
+
+
+        public string Make {  get; set; }
+        public string Model { get; set; } 
+        public int Year { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"The car you are looking at is a {Year}, {Make}, {Model}");  
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Classes
+﻿using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cars myCar = new Cars();
+            myCar.Make = "Honda";
+            myCar.Make = "Civic";
+            myCar.Year = 2019;
+
+            myCar.Print();
+
         }
+       
     }
 }
